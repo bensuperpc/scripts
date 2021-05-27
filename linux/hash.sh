106d94df42e78ed4953b3baf0baa354e52b96408
Bensuperpc <bensuperpc@gmail.com>
2021-05-28 01:43:21 +0200

    Add hash script
    Add hash script
    
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

---
+++ linux/hash.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#//////////////////////////////////////////////////////////////
+#//   ____                                                   //
+#//  | __ )  ___ _ __  ___ _   _ _ __   ___ _ __ _ __   ___  //
+#//  |  _ \ / _ \ '_ \/ __| | | | '_ \ / _ \ '__| '_ \ / __| //
+#//  | |_) |  __/ | | \__ \ |_| | |_) |  __/ |  | |_) | (__  //
+#//  |____/ \___|_| |_|___/\__,_| .__/ \___|_|  | .__/ \___| //
+#//                             |_|             |_|          //
+#//////////////////////////////////////////////////////////////
+#//                                                          //
+#//  Script, 2021                                            //
+#//  Created: 28, May, 2021                                  //
+#//  Modified: 28, May, 2021                                 //
+#//  file: -                                                 //
+#//  -                                                       //
+#//  Source: -                                               //
+#//  OS: ALL                                                 //
+#//  CPU: ALL                                                //
+#//                                                          //
+#//////////////////////////////////////////////////////////////
+if [ $# -eq 0 ]
+then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+TYPE=sha512
+#find $1 -type f -name "*" ! -name "checksums.*" -exec ${TYPE}sum {} \; > checksums.${TYPE}
+#${TYPE}sum -c checksums.${TYPE}
+find $1 -type f -name "*" ! -name "checksums.*" | parallel -j $(nproc) ${TYPE}sum > checksums.${TYPE}
+cat checksums.${TYPE} | parallel --pipe -N1 -j $(nproc) ${TYPE}sum --quiet -c -
+echo "OK"
