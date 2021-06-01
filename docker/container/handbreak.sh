00d05ade02c82e7894c40795545bd7e01ca9339d
Bensuperpc <bensuperpc@gmail.com>
2021-06-01 17:30:29 +0200

    Move docker scripts to :https://github.com/Bensuperpc/docker-scripts.git
    Move docker scripts to :https://github.com/Bensuperpc/docker-scripts.git
    
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- docker/container/handbreak.sh
+++
@@ -1,8 +0,0 @@
-docker run -d \
-    --name=handbrake \
-    -p 5800:5800 \
-    -v /docker/appdata/handbrake:/config:rw \
-    -v $HOME:/storage:ro \
-    -v $HOME/HandBrake/watch:/watch:rw \
-    -v $HOME/HandBrake/output:/output:rw \
-    jlesage/handbrake
--- docker/useful/clean_docker.sh
+++
@@ -1,1 +0,0 @@
-docker system prune
