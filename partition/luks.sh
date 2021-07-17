320a286df5da164ca330cfe8c2b6aebcad2c20dc
Bensuperpc <bensuperpc@gmail.com>
2021-07-18 00:30:39 +0200

    Improve LUKS
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- partition/luks.sh
+++ partition/luks.sh
@@ -35,10 +35,10 @@ fi
 if (( $# == 2 )); then
     UUID=$(uuidgen)
     echo "UUID: ${UUID}"
+    umount $1 || true
     sudo cryptsetup -v --type luks --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 1000 --use-urandom --verify-passphrase luksFormat --label=$2 $1
     sudo cryptsetup luksOpen $1 ${UUID}
-    sudo mkfs.btrfs /dev/mapper/${UUID}
-    sudo mkfs.btrfs -f --label $2 /dev/mapper/${UUID}
+    sudo mkfs.btrfs --force --checksum crc32c --label $2 /dev/mapper/${UUID}
     sudo cryptsetup luksClose ${UUID}
     echo "Partition: OK"
     echo "Now you can unplug, replug device and use it :) "
