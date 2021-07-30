e317503e09c042db3418662f2ad89880847f7c45
Bensuperpc <bensuperpc@gmail.com>
2021-07-30 12:56:40 +0200

    Fix code-inspector violation
    Fix code-inspector violation
    
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- internet/sms-free.py
+++ internet/sms-free.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 #//////////////////////////////////////////////////////////////
 #//   ____                                                   //
 #//  | __ )  ___ _ __  ___ _   _ _ __   ___ _ __ _ __   ___  //
@@ -13,7 +13,7 @@
 #//  Modified: 30, July, 2021                                //
 #//  file: -                                                 //
 #//  -                                                       //
-#//  Source: https://mobile.free.fr/account/mes-options/notifications-sms                                                //
+#//  Source: https://mobile.free.fr/account/mes-options/notifications-sms     //
 #//  OS: ALL                                                 //
 #//  CPU: ALL                                                //
 #//                                                          //
@@ -25,21 +25,25 @@ import sys
 
 if len(sys.argv) <= 3:
     print("Need more arguments: <user> <API key> <Message (in quotes)>")
-    exit(1)
+    sys.exit(1)
 
 user = sys.argv[1]
 key = sys.argv[2]
 msg = sys.argv[3]
 
 payload = { 'user' : user, 'pass' : key, 'msg' : msg}
-url = "https://smsapi.free-mobile.fr/sendmsg?"
+URL = "https://smsapi.free-mobile.fr/sendmsg?"
 
-full_url = url + urllib.parse.urlencode(payload)
+FULL_URL = URL + urllib.parse.urlencode(payload)
 
 try:
-    response = urllib.request.urlopen(full_url)
+    if URL.lower().startswith('https'):
+        req = urllib.request.Request(URL)
+    else:
+        raise ValueError from None
+    response = urllib.request.urlopen(FULL_URL)
     code = response.getcode()
     print('Return code:', code)
 
-except Exception as e:
-    print(f'Error:  {full_url} : {str(e)}')
+except Exception as error:
+    print(f'Error:  {FULL_URL} : {str(error)}')
