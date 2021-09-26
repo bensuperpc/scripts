d223813ce7f579e4e2c6dc3bf8d78ae5d6ea42b2
Bensuperpc <bensuperpc@gmail.com>
2021-09-26 20:38:35 +0200

    Improve NVENC
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- video/gource-nvenc.sh
+++ video/gource-nvenc.sh
@@ -22,8 +22,8 @@
 if (( $# == 1 )); then
     #gource --multi-sampling --output-framerate 60 --seconds-per-day 2.0 --auto-skip-seconds 0.1 ./ -1920x1080 -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec hevc_nvenc -preset slow -b:v 64M -maxrate 96M -bufsize 384M $1
     gource --multi-sampling --output-framerate 60 --seconds-per-day 1.0 \
-    --auto-skip-seconds 0.2 ./ -1920x1080 -o - | ffmpeg -y -r 60 \
-    -f image2pipe -vcodec ppm -i - -vcodec hevc_nvenc -preset:v p7 -tune:v hq -rc:v vbr -cq:v 18 -b:v 0 "$1"
+    --auto-skip-seconds 0.2 ./ -2560x1440 -o - | ffmpeg -y -r 60 \
+    -f image2pipe -vcodec ppm -i - -vcodec hevc_nvenc -preset:v p7 -tune:v hq -rc:v vbr -cq:v 18 -b:v 0 -minrate:v 5M -maxrate:v 30M -bufsize:v 120M "$1"
 else
     echo "Usage: ${0##*/} <ouput file>"
     exit 1
--- video/h265-nvenc.sh
+++ video/h265-nvenc.sh
@@ -10,10 +10,11 @@
 #//                                                          //
 #//  Script, 2020                                            //
 #//  Created: 21, November, 2020                             //
-#//  Modified: 24, July, 2021                                //
+#//  Modified: 26, September, 2021                           //
 #//  file: -                                                 //
 #//  -                                                       //
 #//  Source: https://superuser.com/questions/1236275/how-can-i-use-crf-encoding-with-nvenc-in-ffmpeg/1236387#1236387                                               //
+#//          https://superuser.com/questions/1633516/convert-ffmpeg-encoding-from-libx264-to-h264-nvenc/1650962#1650962
 #//  OS: ALL                                                 //
 #//  CPU: ALL                                                //
 #//                                                          //
@@ -21,6 +22,11 @@
 
 if (( $# == 2 )); then
     # Old CUDA version
-    #ffmpeg -strict 2 -hwaccel auto -i "$1"  -c:v hevc_nvenc -preset slow -rc vbr_hq -cq 18 -qmin 16 -qmax 20 -profile:v main10 -b:v 0K -c:a copy -map 0 "$2"
-    ffmpeg -strict 2 -i "$1" -c:v hevc_nvenc -preset:v p7 -tune:v hq -rc:v vbr -cq:v 18 -b:v 0 -c:a copy -map 0 "$2"
+    #ffmpeg -threads 0 -strict 2 -hwaccel auto -i "$1"  -c:v hevc_nvenc -preset slow -rc vbr_hq -cq 18 -qmin 16 -qmax 20 -profile:v main10 -b:v 0K -c:a copy -map 0 "$2"
+    #-threads 4 -hwaccel cuvid -c:v h264_cuvid
+    ffmpeg -strict 2 \
+        -i "$1" \
+        -c:v hevc_nvenc -gpu:v 0 \
+        -preset:v p7 -tune:v hq -rc:v vbr -cq:v 16 -b:v 0 -minrate:v 5M -maxrate:v 30M -bufsize:v 120M \
+        -c:a copy -map 0 "$2"
 fi
