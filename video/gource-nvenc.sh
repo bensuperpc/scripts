65c46094a6f0f68db0047d6012a32819be11e3cf
Bensuperpc <bensuperpc@gmail.com>
2021-09-29 21:05:00 +0200

    Upgrade quality on gource-nvenc
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- video/gource-nvenc.sh
+++ video/gource-nvenc.sh
@@ -10,10 +10,11 @@
 #//                                                          //
 #//  Script, 2020                                            //
 #//  Created: 21, November, 2020                             //
-#//  Modified: 28, September, 2021                           //
+#//  Modified: 29, September, 2021                           //
 #//  file: -                                                 //
 #//  -                                                       //
 #//  Source: https://gist.github.com/rafi/365926                                               //
+#//          https://superuser.com/a/1296511/1057976
 #//  OS: ALL                                                 //
 #//  CPU: ALL                                                //
 #//                                                          //
@@ -26,7 +27,9 @@ if (($# == 1)); then
     gource --fullscreen --disable-input --multi-sampling --output-framerate 60 --seconds-per-day 0.1 \
         --hide mouse --filename-time 2 --max-files 0 --bloom-multiplier 0.8 --highlight-users --file-extension-fallback --path . \
         --auto-skip-seconds 0.1 --background-colour 000000 --key --stop-at-end --title "$git_url" --output-ppm-stream - | ffmpeg -y -r 60 \
-        -f image2pipe -vcodec ppm -i - -vcodec hevc_nvenc -pix_fmt rgb0 -preset:v p7 -tune:v hq -rc:v vbr -cq:v 12 -b:v 0 -minrate:v 5M -maxrate:v 200M -bufsize:v 800M "$1"
+        -f image2pipe -vcodec ppm -i - -vcodec hevc_nvenc \
+        -bf:v 3 -rc-lookahead:v 32 -refs:v 16 -b_ref_mode:v middle \
+        -pix_fmt rgb0 -preset:v p7 -tune:v hq -rc:v vbr -cq:v 10 -b:v 0 -minrate:v 1M -maxrate:v 400M -bufsize:v 800M "$1"
 else
     echo "Usage: ${0##*/} <ouput file>"
     exit 1
