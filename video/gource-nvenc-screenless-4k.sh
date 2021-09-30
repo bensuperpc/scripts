e1aaff721e46e6d55d270e8dff69401f7c865306
Bensuperpc <bensuperpc@gmail.com>
2021-09-30 14:14:47 +0200

    Add logo file option for gource
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- video/gource-nvenc-screenless-4k.sh
+++ video/gource-nvenc-screenless-4k.sh
@@ -31,35 +31,66 @@
 # FFMPEG -pix_fmt yuv420p yuvj420p rgb0
 
 if (($# == 1)); then
-git_url=$(git config --get remote.origin.url)
-xvfb-run -a -s "-screen 0 3840x2160x24" \
-  gource \
-    --stop-at-end \
-    --fullscreen \
-    --multi-sampling \
-    --seconds-per-day 0.1 \
-    --highlight-users \
-    --max-files 0 \
-    --auto-skip-seconds 0.1 \
-    --background-colour 000000 \
-    --bloom-multiplier 0.8 \
-    --font-scale 1.50 \
-    --key \
-    --filename-time 2 \
-    --file-idle-time 0 \
-    --output-framerate 60 \
-    --hide mouse \
-    --file-extension-fallback \
-    --path . \
-    --output-ppm-stream - \
-    --title "$git_url" \
-    | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - \
-    -vcodec hevc_nvenc -pix_fmt rgb0 \
-    -bf:v 3 -rc-lookahead:v 32 -refs:v 16 -b_ref_mode:v middle \
-    -preset:v p7 -tune:v hq \
-    -rc:v vbr -cq:v 10 -b:v 0 \
-    -minrate:v 1M -maxrate:v 800M -bufsize:v 800M "$1"
+  git_url=$(git config --get remote.origin.url)
+  xvfb-run -a -s "-screen 0 3840x2160x24" \
+    gource \
+      --stop-at-end \
+      --fullscreen \
+      --multi-sampling \
+      --seconds-per-day 0.1 \
+      --highlight-users \
+      --max-files 0 \
+      --auto-skip-seconds 0.1 \
+      --background-colour 000000 \
+      --bloom-multiplier 0.8 \
+      --font-scale 1.50 \
+      --key \
+      --filename-time 2 \
+      --file-idle-time 0 \
+      --output-framerate 60 \
+      --hide mouse \
+      --file-extension-fallback \
+      --path . \
+      --output-ppm-stream - \
+      --title "$git_url" \
+      | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - \
+      -vcodec hevc_nvenc -pix_fmt rgb0 \
+      -bf:v 3 -rc-lookahead:v 32 -refs:v 16 -b_ref_mode:v middle \
+      -preset:v p7 -tune:v hq \
+      -rc:v vbr -cq:v 10 -b:v 0 \
+      -minrate:v 1M -maxrate:v 800M -bufsize:v 800M "$1"
+elif (($# == 2)); then
+  git_url=$(git config --get remote.origin.url)
+  xvfb-run -a -s "-screen 0 3840x2160x24" \
+    gource \
+      --stop-at-end \
+      --fullscreen \
+      --multi-sampling \
+      --seconds-per-day 0.1 \
+      --highlight-users \
+      --max-files 0 \
+      --auto-skip-seconds 0.1 \
+      --background-colour 000000 \
+      --bloom-multiplier 0.8 \
+      --font-scale 1.50 \
+      --key \
+      --filename-time 2 \
+      --file-idle-time 0 \
+      --output-framerate 60 \
+      --hide mouse \
+      --file-extension-fallback \
+      --logo "$1" \
+      --path . \
+      --output-ppm-stream - \
+      --title "$git_url" \
+      | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - \
+      -vcodec hevc_nvenc -pix_fmt rgb0 \
+      -bf:v 3 -rc-lookahead:v 32 -refs:v 16 -b_ref_mode:v middle \
+      -preset:v p7 -tune:v hq \
+      -rc:v vbr -cq:v 10 -b:v 0 \
+      -minrate:v 1M -maxrate:v 800M -bufsize:v 800M "$2"
 else
     echo "Usage: ${0##*/} <ouput file>"
+    echo "       ${0##*/} <logo file> <ouput file>"
     exit 1
 fi
